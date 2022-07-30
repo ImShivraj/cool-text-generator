129abe3c956d8479038726ec99dbe8d3f05725dd
Shivraj <shivunofficial003@gmail.com>
2022-07-30 19:52:59 +0530

    adding js

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 
+function takeshot() {
+  let div = document.getElementById("wrapper");
+  //   console.log(div);
+  let result = document.getElementById("photo");
+  html2canvas(div).then(function (canvas) {
+    document.getElementById("output").appendChild(canvas);
+  });
+//   result.addEventListener("click", () => (location.href = "#result"));
 
+  
+    
+}
